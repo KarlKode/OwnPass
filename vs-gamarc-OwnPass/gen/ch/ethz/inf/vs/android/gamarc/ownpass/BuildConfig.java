0c8cabf23ea2bb6f36c7919e75a9d017b4bacd5e
Valmendil <travencia@hotmail.com>
2013-12-16 16:08:44 +0100

    start

---
+++ vs-gamarc-OwnPass/gen/ch/ethz/inf/vs/android/gamarc/ownpass/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ch.ethz.inf.vs.android.gamarc.ownpass;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
