dd035c35049ec17dccded21cb228178def15a801
Karl Kode <marc@marcg.ch>
2013-12-19 15:31:15 +0100

    Fixed errors by me

--- vs-gamarc-OwnPass/src/ch/ethz/inf/vs/android/gamarc/ownpass/Password.java
+++ vs-gamarc-OwnPass/src/ch/ethz/inf/vs/android/gamarc/ownpass/Password.java
@@ -5,18 +5,29 @@ import android.util.Base64;
 public class Password {
     private long id;
     private Server server;
+    private long serverId;
     private String title;
     private String url;
     private byte[] username;
     private byte[] password;
 
-    public Password(long id, Server server, String title, String url, byte[] username, byte[] password) {
+    public Password(Server server, long serverId, String title, String url, byte[] username, byte[] password) {
         setId(id);
         setServer(server);
+        setServerId(serverId);
         setTitle(title);
         setUrl(url);
         setUsername(username);
         setPassword(password);
+    }
+
+    public Password(Server server, long serverId, String title, String url, String username, String password) {
+        this(server, serverId, title, url, Base64.decode(username, Base64.DEFAULT), Base64.decode(password, Base64.DEFAULT));
+    }
+
+    public Password(long id, Server server, long serverId, String title, String url, byte[] username, byte[] password) {
+        this(server, serverId, title, url, username, password);
+        setId(id);
     }
 
     public long getId() {
@@ -33,6 +44,14 @@ public class Password {
 
     public void setServer(Server server) {
         this.server = server;
+    }
+
+    public long getServerId() {
+        return serverId;
+    }
+
+    public void setServerId(long serverId) {
+        this.serverId = serverId;
     }
 
     public String getTitle() {
--- vs-gamarc-OwnPass/src/ch/ethz/inf/vs/android/gamarc/ownpass/PasswordUpdateRequest.java
+++ vs-gamarc-OwnPass/src/ch/ethz/inf/vs/android/gamarc/ownpass/PasswordUpdateRequest.java
@@ -1,105 +1,89 @@
 package ch.ethz.inf.vs.android.gamarc.ownpass;
 
 import android.os.AsyncTask;
-import android.util.Base64;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 public class PasswordUpdateRequest extends AsyncTask<String, Void, String> {
-	private	ArrayList<Password> passwords = new ArrayList<Password>();
-	private String authorization;
-	
-	public PasswordUpdateRequest(String authorization) {
-		this.authorization = authorization;
-	}
-	
-	@Override
-	protected String doInBackground(String... params) { // URL
-	    StringBuilder builder = new StringBuilder();
-       //ResponseHandler<String> responseHandler = new BasicResponseHandler();
+    private ArrayList<Password> passwords = new ArrayList<Password>();
+    private String authorization;
+    private Server server;
+
+    public PasswordUpdateRequest(Server server, String authorization) {
+        this.server = server;
+        this.authorization = authorization;
+    }
+
+    private String getUrl() {
+        return server.getUrl() + "/passwords";
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        StringBuilder builder = new StringBuilder();
         DefaultHttpClient httpClient = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(params[0]+"/passwords");
-        
+        HttpGet httpGet = new HttpGet(getUrl());
+
         httpGet.setHeader("Content-Type", "application/json");
         httpGet.setHeader("Authorization", authorization);
 
         try {
-          HttpResponse  httpResponse = httpClient.execute(httpGet);
+            HttpResponse httpResponse = httpClient.execute(httpGet);
 
-	        HttpEntity entity = httpResponse.getEntity();
-	        InputStream content = entity.getContent();
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-	        String line;
-	        while ((line = reader.readLine()) != null) {
-	          builder.append(line);
-	        }
-	    } catch (ClientProtocolException e) {
-	      e.printStackTrace();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }
-	    
-	    return builder.toString();
-	}
+            HttpEntity entity = httpResponse.getEntity();
+            InputStream content = entity.getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	@Override
-	protected void onPostExecute(String result) {
-		JSONArray pws;
-		try {
-			pws = new JSONArray(result);
-			for (int i=0; i < pws.length(); i++)
-			{
-				JSONObject oneObject = pws.getJSONObject(i);
-			    // Pulling items from the array
-			    int id = oneObject.getInt("id");
-			    String pw = oneObject.getString("password");
-			    String url = oneObject.getString("url");
-			    int uid = oneObject.getInt("user_id");
-			    String name = oneObject.getString("username");
-			    String title = oneObject.getString("title");
-			    
-			    passwords.add(new Password(uid, id, url, title, name, pw));
-			}
-		} catch (JSONException e) {
-			Log.e(PasswordUpdateRequest.class.toString(), "Failed to download file");
-		}
-		
-		//TODO callback
-	}
-	
-	public ArrayList<Password> getPasswords(){
-		return passwords;
-	}	
+        return builder.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        JSONArray pws;
+        try {
+            pws = new JSONArray(result);
+            for (int i = 0; i < pws.length(); i++) {
+                JSONObject oneObject = pws.getJSONObject(i);
+                // Pulling items from the array
+                int id = oneObject.getInt("id");
+                String pw = oneObject.getString("password");
+                String url = oneObject.getString("url");
+                String name = oneObject.getString("username");
+                String title = oneObject.getString("title");
+
+                passwords.add(new Password(server, id, url, title, name, pw));
+            }
+        } catch (JSONException e) {
+            Log.e(PasswordUpdateRequest.class.toString(), "Failed to download file");
+        }
+
+        //TODO callback
+    }
+
+    public ArrayList<Password> getPasswords() {
+        return passwords;
+    }
 }
