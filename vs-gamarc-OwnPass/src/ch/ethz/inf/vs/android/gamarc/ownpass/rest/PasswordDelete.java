05ad753cf4e820c4cee4ab309b7556933427a4aa
Karl Kode <marc@marcg.ch>
2013-12-19 17:55:55 +0100

    Rewrote the other REST stuff

--- vs-gamarc-OwnPass/src/ch/ethz/inf/vs/android/gamarc/ownpass/rest/PasswordDelete.java
+++ vs-gamarc-OwnPass/src/ch/ethz/inf/vs/android/gamarc/ownpass/rest/PasswordDelete.java
@@ -5,60 +5,61 @@ import android.util.Log;
 import ch.ethz.inf.vs.android.gamarc.ownpass.Password;
 import ch.ethz.inf.vs.android.gamarc.ownpass.PasswordUpdateRequest;
 import ch.ethz.inf.vs.android.gamarc.ownpass.Server;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PasswordDelete extends AsyncTask<Password, Void, String> {
 
     private Server server;
     private String authorizationString;
-    private ArrayList<Password> pToUp;
 
     public PasswordDelete(Server server, String authorizationString) {
         this.server = server;
         this.authorizationString = authorizationString;
     }
 
-    private String getUrl() {
-        return server.getUrl() + "/passwords";
+    private String getUrl(Password password) {
+        return server.getUrl() + "passwords/" + password.getServerId();
     }
 
     @Override
     protected String doInBackground(Password... params) {
-        String result = "";
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpDelete httpDelete;
+        httpDelete = new HttpDelete(getUrl(params[0]));
+        httpDelete.setHeader("Content-Type", "application/json");
+        httpDelete.setHeader("Authorization", authorizationString);
+
         try {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpDelete httpDelete = new HttpDelete(getUrl() + "/" + params[0].getServerId());
-
-            httpDelete.setHeader("Content-type", "application/json");
-            httpDelete.setHeader("Authorization", authorizationString);
-
-            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-            result = httpclient.execute(httpDelete, responseHandler);
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
+            HttpResponse response = httpClient.execute(httpDelete);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                return null;
+            }
+            return EntityUtils.toString(response.getEntity(), "UTF-8");
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return result;
+
+        return null;
     }
 
     @Override
     protected void onPostExecute(String response) {
-        // handle response;
-        try {
-            JSONObject oneObject = new JSONObject(response);
-            String login = oneObject.getString("status");
-            if (login.equals("password_deleted")) ;
-            //TODO remove passwordToUpdate
-        } catch (JSONException e) {
-            Log.e(PasswordUpdateRequest.class.toString(), "Failed to download file: " + e.getMessage());
-        }
+        // TODO: Handle response
     }
 
 }
